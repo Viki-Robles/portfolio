c50c5439369dd6d4d06018f0fd277131d05a165e
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-29 21:19:33 +0000

    button js

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,16 +4,23 @@ import './NavBar.css';
 
 
 export default function NavBar() {
+    const [menuDisplay, setMenuDisplay] = React.useState("menuDisplayoff");
+
+function toggleMenu() {
+        setMenuDisplay(menuDisplay === "menuDisplayoff" ? ""
+        : "menuDisplayoff");
+    }
+
     return (
         <div>
            <nav className="myNav">
-            <button className="burgerButton">
+            <button className="burgerButton" onClick={() => toggleMenu()}>
                 <img className ="burger"src={hamburger} alt="hamburger"/>
             </button>
             </nav>
            
 
-           <main className="navMenu">
+           <main className="navMenu" id={menuDisplay}>
             <h4 className="menuItem"><a href="/">home</a></h4>
             <h4 className="menuItem"><a href="/portfolio">portfolio</a></h4>
             <h4 className="menuItem"><a href="/services">services</a></h4>

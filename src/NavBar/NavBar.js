06b9066a30ccab876033aafd452c1c0ad47bef5b
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-02-29 20:53:59 +0000

    button

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,10 +6,20 @@ import './NavBar.css';
 export default function NavBar() {
     return (
         <div>
-           <main className="myNav">
-            <button>
+           <nav className="myNav">
+            <button className="burgerButton">
                 <img className ="burger"src={hamburger} alt="hamburger"/>
             </button>
+           </nav>
+
+           <main className="navMenu">
+            <h4><a href="/">home</a></h4>
+            <h4><a href="/">portfolio</a></h4>
+            <h4><a href="/">services</a></h4>
+            <h4><a href="/">contact</a></h4>
+
+
+
            </main>
         </div>
     );

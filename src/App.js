3f8617b0a9f12e86030322da06e13e7c1638804b
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-03-01 19:18:52 +0000

    building the content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import NavBar from './NavBar/NavBar';
+import MainMenu from './MainMenu/MainMenu';
 
 
 function App() {
   return (
+
     <div className="App">
-    
-    <div className="header">
+      <div className="header">
         Rubie
       </div>
-     <NavBar/>
-     
+      <NavBar />
+      <MainMenu/>
     </div>
+
   );
 }
 

20056fe7dabac20a63ca857824f531dfbe7c7780
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-03-13 22:41:25 +0000

    preparing the last 2 services cards

--- src/CardServices/CardServices.js
+++ src/CardServices/CardServices.js
@@ -4,13 +4,12 @@ import './CardServices.css';
 export default function CardServices(props) {
     return (
         <>
-            
-                <div className="pricingBox">{props.pricingBox}
+            <div className="pricingBox">{props.pricingBox}
+                <div className="servicesBox">
                     <h2>{props.titlePricing}</h2>
                     <div className="">{props.detailsPricing}</div>
                 </div>
-            
-
+            </div>
         </>
     );
 }

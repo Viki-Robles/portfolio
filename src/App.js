b720c608675f35d94d1ac72934d863e61edfd9b6
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-03-17 20:57:02 +0000

    adding footer and Router to the app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar/NavBar';
 import Gallery from './Container/Gallery';
 import GetPricing from './CardServices/getPricing';
 import HomeButton from './homeButton/homeButton';
-
+import ScrollToTop from './ScrollMenu/ScrollMenu';
+import Footer from './Footer/Footer';
 
 
 function App() {
   return (
-
+  <Router>
+    <ScrollToTop/>
     <div className="App">
       <div className="header">
         Rubie
-      </div>
+        </div>
       <NavBar />
-      <div className="portfolio_name">PORTFOLIO</div>
+        <div className="portfolio_name">PORTFOLIO</div>
       <Gallery />
-      <br />
-      <hr />
-      <br />
-      <br />
-
-      <div className="Services">SERVICES
+        <br />
+        <hr />
+        <br />
+        <br />
+        <div className="Services">SERVICES
       <GetPricing />
-      </div>
-      <div className="ContactForm">Get In Touch</div>
+        </div>
+        <div className="ContactForm">Get In Touch</div>
       <HomeButton />
+      <Footer/>
     </div>
-
+</Router>
   );
 }
 
---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Footer () {
+    return (
+        <div className="footer">
+            <item>Linkedin</item>
+            <item>GitHub</item>
+            <item>Email</item>
+        </div>
+    );
+}

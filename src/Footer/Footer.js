1c30ffc9d641d38ae58d78f7c5b7bcfc706553c2
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-03-22 12:04:49 +0000

    UI  cards

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Linkedin_icon from '../images/Linkedin_icon.png';
-import github from '../images/github.png';
+
 
 
 
@@ -9,10 +8,8 @@ export default function Footer() {
         <>
         <div className="footer">
             <item>
-                <img className="footer_icon" src={Linkedin_icon} />
                 <a href="https://www.linkedin.com/in/vicky-vasilopoulou-52230111b/">Linkedin</a></item>
             <item>
-                <img className="footer_icon" src={github} />
                 <a href="https://github.com/Viki-Robles">GitHub</a></item>
             <item>Email</item>
             </div>
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -15,10 +15,12 @@ export default class Form extends Component {
         return (
             <>
                 <form>
-                    <input placeholder="First Name: " value={this.state.firstName} 
-                    onChange={e => this.setState({firstname: e.target.value})}/>
-                    <input placeholder="Last Name: " value={this.state.lastName}></input>
-                    <input placeholder="Email: " value={this.state.email}></input>
+                    <input placeholder="First Name " value={this.state.firstName} 
+                    onChange={e => this.setState({firstName: e.target.value})}/>
+                    <input placeholder="Last Name " value={this.state.lastName}
+                    onChange={e => this.setState({lastName: e.target.value})}/>
+                    <input placeholder="Email " value={this.state.email}
+                    onChange={e => this.setState({email: e.target.value})}/>
                 </form>
 
             </>

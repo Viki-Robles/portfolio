8f4b3b9da39bbe87c195932ba73a26fe83bf6d85
Vicky <vicky@Vickys-MBP.default>
2020-04-06 19:55:33 +0100

    social media buttons where fixed

--- src/images/AboutMe/AboutMe.js
+++ src/images/AboutMe/AboutMe.js
@@ -6,8 +6,13 @@ export default function AboutMe() {
     return (
         <div className="square">
             <div className="content">
-                <h3>About Me</h3>
-                <p></p>
+                <p> <h1>Hi, my name is</h1><br/> <h2>Viki Vasilopoulou</h2> 
+                 and I am a web developer with passion for front end development and design. 
+                    I recently finished a bootcamp called Kodiri 
+                    and I am aspire towards a career that will alow 
+                    me to channel my creativity through crafting 
+                    beautiful software and engaging experiences. </p>
+                
             </div>
         </div>
     );

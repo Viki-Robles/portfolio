dd2379287652a23db01577a558ec316415d039c9
Vicky <vicky@Vickys-MBP.default>
2020-04-06 21:40:33 +0100

    hovering effect on projects

--- src/images/AboutMe/AboutMe.js
+++ src/images/AboutMe/AboutMe.js
@@ -6,12 +6,11 @@ export default function AboutMe() {
     return (
         <div className="square">
             <div className="content">
-                <p> <h1>Hi, my name is</h1><br/> <h2>Viki Vasilopoulou</h2> 
-                 and I am a web developer with passion for front end development and design. 
-                    I recently finished a bootcamp called Kodiri 
-                    and I am aspire towards a career that will alow 
-                    me to channel my creativity through crafting 
-                    beautiful software and engaging experiences. </p>
+                <p> <h1>Hi, my name is</h1><br/> <h2>Vasiliki Vasilopoulou</h2>and I am a web developer with passion for front-end development and design. 
+                 <br/>I recently finished the Bootcamp called Kodiri 
+                    and I am aspire towards a career <br/>that will alow 
+                    me to channel my creativity through crafting
+                    beautiful <br/>software and engaging experiences. </p>
                 
             </div>
         </div>

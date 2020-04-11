360328d466a5aeaa705764b47b8bb006c28dbaba
Vicky <vicky@Vickys-MBP.default>
2020-04-11 23:13:44 +0100

    UI updates

--- src/CardServices/CardServices.js
+++ src/CardServices/CardServices.js
@@ -7,7 +7,7 @@ export default function CardServices(props) {
         <>
             <div className="pricingBox">{props.pricingBox}
                 <div className="servicesBox">
-                    <h2>{props.titlePricing}</h2>
+                    <h2 className="titlePricing">{props.titlePricing}</h2>
                     <img className="icon" src={props.icon} alt="icon"/>
                     <p className="pricingFeauture">{props.pricingFeauture}</p>
                 </div>

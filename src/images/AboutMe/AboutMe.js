d2a71098304fe113121ba46bf99e29b21c09b9b7
Vicky <vicky@Vickys-MBP.default>
2020-04-11 17:41:08 +0100

    new changes

--- src/images/AboutMe/AboutMe.js
+++ src/images/AboutMe/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AboutMe.css';
-import AOS from 'aos';
+import vikipeki from '../../MyPic/vikipeki.png';
+
 
 
 export default function AboutMe() {
@@ -8,21 +9,33 @@ export default function AboutMe() {
 
 
     return (
-        <div className="hexagon">
-            <div className="hexagon-inside">
-                <div className="hexagon-content">
-                    <h2 className="questionMark">?</h2>
-                <h1>Hi, my name is</h1><br /> <h2>Vasiliki Vasilopoulou</h2>and I 
-                    am a web developer with passion for front-end development and design.
-                    I recently finished the Bootcamp called Kodiri
-                    and currently I am aspired towards a career that will alow
-                    me to channel my creativity through crafting
-                    beautiful software and engaging experiences.
+        <>
+            <div className="hexagon">
+                <div className="hexagon-inside">
+                    <div className="hexagon-content">
+                        <h2 className="questionMark">?</h2>
+                        <h1>Hi, my name is</h1><br /> <h2>Vasiliki Vasilopoulou</h2>and I
+                            am a web developer with passion for front-end development and design.
+                            I recently finished the Bootcamp called Kodiri
+                            and currently I am aspired towards a career that will alow
+                            me to channel my creativity through crafting
+                            beautiful software and engaging experiences.
                 </div>
 
+                </div>
             </div>
-        </div>
+
+            <div className="hexagonPic">
+                <div className="hexagon_insidePic">
+                    <div className="hexagon-contentPic">
+                            
+                    </div>
+
+                </div>
+            </div>
+
+        </>
     );
 }
 
-/**data-aos="fade-down" */
+/**<img src={vikipeki} />*/

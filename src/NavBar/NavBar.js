f89dfea43b964f3aa2e33749450f454863a8ad72
Vicky <vicky@Vickys-MBP.default>
2020-04-18 12:17:04 +0100

    updates on the code

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -17,17 +17,17 @@ export default function NavBar() {
         </button>
       </nav>
       <main className="navMenu" id={menuDisplay}>
-        <h4 className="menuItem">
+        <h4 className="navItem">
           <a href="#portfolio" onClick={() => toggleMenu()}>
             1. portfolio
           </a>
         </h4>
-        <h4 className="menuItem">
+        <h4 className="navItem">
           <a href="#services" onClick={() => toggleMenu()}>
             2. services
           </a>
         </h4>
-        <h4 className="menuItem">
+        <h4 className="navItem">
           <a href="#myDetails" onClick={() => toggleMenu()}>
             3. contact
           </a>
